5ec932355532847c70dd2a4a858a177ef1e34e67
MartinFrith <telemagico@gmail.com>
2015-10-05 19:28:04 -0300

    Config database defaults

--- config/config.sample.php
+++ config/config.sample.php
@@ -3,10 +3,10 @@
 $config['languages'] = array('en');
 
 $config['database'] = array(
-	'dns' => "mysql:host=127.0.0.1;port=3306;dbname=caldero",
+	'dns' => "mysql:host=127.0.0.1;port=3306;dbname=bootie;charset=utf8",
 	'username' => 'root',
-	'password' => ($local ? 'localpass' : 'serverpass'),
-	//'dns' => "pgsql:host=localhost;port=5432;dbname=micromvc",
+	'password' => '',
+	//'dns' => "pgsql:host=localhost;port=5432;dbname=bootie",
 	//'username' => 'postgres',
 	//'password' => 'postgres',
 	'params' => array()
