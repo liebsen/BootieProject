3376d70859ece0ab073f87182bc7228739828087
MartinFrith <telemagico@gmail.com>
2015-10-23 09:48:13 -0300

    Config sample update

--- config/config.sample.php
+++ config/config.sample.php
@@ -1,13 +1,62 @@
-<?php
-// Available translations (Array of Locales)
-$config['languages'] = array('en');
+<?php 
 
-$config['database'] = array(
-	'dns' => "mysql:host=127.0.0.1;port=3306;dbname=bootie;charset=utf8",
-	'username' => 'root',
-	'password' => '',
-	//'dns' => "pgsql:host=localhost;port=5432;dbname=bootie",
-	//'username' => 'postgres',
-	//'password' => 'postgres',
-	'params' => array()
-);
+$config = [
+	/* 
+	 * Available translations (Array of Locales)
+	 */
+
+	'debug' => false,
+	
+	'languages' => ['en'],
+
+	'cache_enabled' => false, 
+
+	/* 
+	 * Cache settings
+	 */
+
+	'cache' => [
+		'cache_ext' => '.cache', //file extension
+		'cache_time' => 3600,  //Cache file expires afere these seconds (1 hour = 3600 sec)
+		'cache_folder' => SP . 'storage/cache/', //folder to store Cache files
+		'ignore_pages' => [
+			'/login',
+			'/register',
+			'/logout',
+			'/admin/(.*)'
+		]
+	],
+
+	/* 
+	 * Image manipulation (In Pixels)
+	 */
+
+	'img_sizes' => [
+	    'tiny'  => [75,75],
+	    'thumb' => [225,225],
+	    'std'   => [600,400]
+	],
+
+	'img_save_orig' => true,
+
+	/* 
+	 * Database Access Credentials
+	 */
+
+	'database' => [
+		'connections' => [
+			'default' => [
+				'dns' => "mysql:host=127.0.0.1;port=3306;dbname=bootie;charset=utf8",
+				'username' => 'mysql',
+				'password' => 'mysql',
+				'params' => array()
+			],
+			'postgres' => [
+				'dns' => "pgsql:host=localhost;port=5432;dbname=bootie;charset=utf8",
+				'username' => 'postgres',
+				'password' => 'postgres',
+				'params' => array()
+			]
+		]
+	]
+];
