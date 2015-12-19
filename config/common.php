d80aa926d34426077cd00beca324b88cd8beb339
MartinFrith <telemagico@gmail.com>
2015-12-18 21:59:13 -0300

    common fix

--- config/common.php
+++ config/common.php
@@ -519,46 +519,6 @@ function directory_is_writable($dir, $chmod = 0755)
 	return TRUE;
 }
 
-
-/**
- * Convert any given variable into a SimpleXML object
- *
- * @param mixed $object variable object to convert
- * @param string $root root element name
- * @param object $xml xml object
- * @param string $unknown element name for numeric keys
- * @param string $doctype XML doctype
- */
-function to_xml($object, $root = 'data', $xml = NULL, $unknown = 'element', $doctype = "<?xml version = '1.0' encoding = 'utf-8'?>")
-{
-	if(is_null($xml))
-	{
-		$xml = simplexml_load_string("$doctype<$root/>");
-	}
-
-	foreach((array) $object as $k => $v)
-	{
-		if(is_int($k))
-		{
-			$k = $unknown;
-		}
-
-		if(is_scalar($v))
-		{
-			$xml->addChild($k, h($v));
-		}
-		else
-		{
-			$v = (array) $v;
-			$node = array_diff_key($v, array_keys(array_keys($v))) ? $xml->addChild($k) : $xml;
-			self::from($v, $k, $node);
-		}
-	}
-
-	return $xml;
-}
-
-
 /**
  * Return an IntlDateFormatter object using the current system locale
  *
